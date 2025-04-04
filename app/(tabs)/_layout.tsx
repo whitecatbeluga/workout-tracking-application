37748f8d49d59146ca721bb8df51c3fa99410e5e
Chadie Gil <chadie.augis@jg-corporation.com>
2025-04-04 12:32:30 +0800

    update: navbar bottom

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,72 +1,15 @@
-import { Tabs } from "expo-router";
 import React from "react";
-import { Platform, Pressable } from "react-native";
+import { Tabs } from "expo-router";
+import { TabBar } from "@/components/tab-bar";
 
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Ionicons } from "@expo/vector-icons";
-import { View } from "react-native";
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: (props) => (
-          <Pressable
-            {...props}
-            android_ripple={{ borderless: true, color: "transparent" }}
-            style={({ pressed }) => [
-              {
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                opacity: pressed ? 0.9 : 1,
-              },
-            ]}
-          />
-        ),
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            position: "absolute",
-          },
-          default: {},
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="workout"
-        options={{
-          title: "Workout",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="barbell" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="person-outline" size={24} color={color} />
-          ),
-        }}
-      />
+    <Tabs tabBar={(props) => <TabBar {...props} />}>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="workout" options={{ title: "Workout" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
   );
-}
+};
+
+export default TabLayout;
---
+++ components/tab-bar.tsx
@@ -0,0 +1,87 @@
+import { View, Text, StyleSheet } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import TabBarButton from "./tab-bar-button";
+
+export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+  let labelContent;
+
+  return (
+    <View style={styles.tabBar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        if (typeof label === "function") {
+          labelContent = label({
+            focused: isFocused,
+            color: isFocused ? "#673ab7" : "#222",
+            position: "below-icon",
+            children: "",
+          });
+        } else {
+          labelContent = (
+            <Text style={{ color: isFocused ? "#673ab7" : "#222" }}>
+              {label}
+            </Text>
+          );
+        }
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TabBarButton
+            key={route.name}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            // style={styles.tabBarItem}
+            isFocused={isFocused}
+            routeName={route.name}
+            color={isFocused ? "#673ab7" : "#222"}
+            label={labelContent}
+          />
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: "absolute",
+    bottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginHorizontal: 24,
+    paddingVertical: 10,
+    borderRadius: 35,
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+  },
+});
