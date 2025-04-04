7c140160ca55df801b96b6a784c1d9f80fd8e467
Chadie Gil <chadie.augis@jg-corporation.com>
2025-04-04 17:22:24 +0800

    update

--- constants/NavBarIcons.tsx
+++
@@ -1,9 +0,0 @@
-import { Feather, FontAwesome5 } from "@expo/vector-icons";
-
-export const icon = {
-  index: (props: any) => <Feather name="home" size={24} {...props} />,
-  workout: (props: any) => (
-    <FontAwesome5 name="dumbbell" size={24} {...props} />
-  ),
-  profile: (props: any) => <Feather name="user" size={24} {...props} />,
-};
