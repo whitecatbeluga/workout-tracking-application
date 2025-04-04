8111681e352f24a6314591ab77288ff81d5041f8
Chadie Gil <chadie.augis@jg-corporation.com>
2025-04-04 12:32:30 +0800

    update: navbar bottom

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,21 +1,20 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
-const Home = () => {
+const Profile = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Profile</Text>
     </View>
   );
 };
 
-export default Home;
+export default Profile;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:'gray'
   },
 });
---
+++ components/tab-bar-button.tsx
@@ -0,0 +1,80 @@
+import { Pressable, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { icon } from "@/constants/NavBarIcons";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+type TabBarButtonProps = {
+  onPress: () => void;
+  onLongPress: () => void;
+  isFocused: boolean;
+  routeName: string;
+  color: string;
+  label: string | React.ReactNode;
+};
+
+const TabBarButton = ({
+  onPress,
+  onLongPress,
+  isFocused,
+  routeName,
+  color,
+  label,
+}: TabBarButtonProps) => {
+  const iconName = routeName as keyof typeof icon;
+  const scale = useSharedValue(0);
+  useEffect(() => {
+    scale.value = withSpring(
+      typeof isFocused === "boolean" ? (isFocused ? 1 : 0) : isFocused,
+      { duration: 350 }
+    );
+  }, [scale, isFocused]);
+
+  const animateIconStyle = useAnimatedStyle(() => {
+    const scaleValue = interpolate(scale.value, [0, 1], [1, 1.2]);
+    const top = interpolate(scale.value, [0, 1], [0, 9]);
+    return {
+      transform: [{ scale: scaleValue }],
+      top,
+    };
+  });
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(scale.value, [0, 1], [1, 0]);
+    return {
+      opacity,
+    };
+  });
+
+  return (
+    <Pressable
+      onPress={onPress}
+      onLongPress={onLongPress}
+      style={styles.tabBarItem}
+    >
+      <Animated.View style={animateIconStyle}>
+        {icon[iconName]({ color: isFocused ? color : "#222" })}
+      </Animated.View>
+      <Animated.Text
+        style={[{ color: isFocused ? color : "#222" }, animatedTextStyle]}
+      >
+        {label}
+      </Animated.Text>
+    </Pressable>
+  );
+};
+
+export default TabBarButton;
+
+const styles = StyleSheet.create({
+  tabBarItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+});
