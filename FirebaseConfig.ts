6f12db0e64c1c2f05ac1ba572975312f64b6509b
Philippe <philippe.tan@jg-corporation.com>
2025-04-24 09:39:49 +0800

    [perf] updated

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,16 +1,14 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
-import {
-  initializeAuth,
-  getAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const API_URL = (Constants.expoConfig?.extra as { API_URL: string }).API_URL;
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBTMjc6SLDQFATn5IrJ0TSkC1W0A68ZjOc",
+  apiKey: (Constants.expoConfig?.extra as { API_KEY: string }).API_KEY,
   authDomain: "workout-tracking-app-e7a2d.firebaseapp.com",
   projectId: "workout-tracking-app-e7a2d",
   storageBucket: "workout-tracking-app-e7a2d.firebasestorage.app",
@@ -20,20 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-let auth;
-try {
-  auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
-  });
-} catch (error) {
-  if (error.code === "auth/already-initialized") {
-    auth = getAuth(app);
-  } else {
-    throw error;
-  }
-}
-
+const auth = getAuth();
 const db = getFirestore(app);
 const storage = getStorage(app);
 
