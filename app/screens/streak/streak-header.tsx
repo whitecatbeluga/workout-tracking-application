5f9c225c5048273035799f915e2f245bb42df0ea
irvin.larawan <irvinhalen.larawan@gmail.com>
2025-04-25 16:47:12 +0800

    streak calendar ui now displays images

--- app/screens/streak/streak-header.tsx
+++ app/screens/streak/streak-header.tsx
@@ -1,24 +1,64 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Modal from "react-native-modal";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const StreakHeader = () => {
+  const [streakModal, setStreakModal] = useState(false);
+  const [restModal, setRestModal] = useState(false);
+
   return (
     <View style={styles.headerContainer}>
-      <View style={styles.streakContainer}>
+      <TouchableOpacity
+        style={styles.streakContainer}
+        onPress={() => setStreakModal(true)}
+      >
         <MaterialCommunityIcons name="fire" size={45} color="#DC723D" />
         <View>
           <Text style={styles.streakText}>15 Weeks</Text>
           <Text style={styles.streakLabel}>Streak</Text>
         </View>
-      </View>
-      <View style={styles.streakContainer}>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.streakContainer}
+        onPress={() => setRestModal(true)}
+      >
         <MaterialCommunityIcons name="power-sleep" size={45} color="#48A6A7" />
         <View>
           <Text style={styles.streakText}>1 Days</Text>
           <Text style={styles.streakLabel}>Rest</Text>
         </View>
-      </View>
+      </TouchableOpacity>
+      <Modal
+        isVisible={streakModal}
+        onBackdropPress={() => setStreakModal(false)}
+      >
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>
+            You worked out 18 days within your streak of 15 weeks.{"\n\n"}Make
+            sure to log a workout at least once a week to keep the streak going.
+          </Text>
+          <TouchableOpacity
+            style={styles.modalButton}
+            onPress={() => setStreakModal(false)}
+          >
+            <Text style={styles.modalButtonText}>Okay</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <Modal isVisible={restModal} onBackdropPress={() => setRestModal(false)}>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>
+            It has been 1 day since your last workout.
+          </Text>
+          <TouchableOpacity
+            style={styles.modalButton}
+            onPress={() => setRestModal(false)}
+          >
+            <Text style={styles.modalButtonText}>Okay</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -54,5 +94,31 @@ const styles = StyleSheet.create({
     color: "#808080",
     fontSize: 16,
     fontFamily: "Inter_400Regular",
+  },
+  modalContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    gap: 20,
+  },
+  modalText: {
+    textAlign: "center",
+    fontFamily: "Inter_600",
+    fontSize: 18,
+  },
+  modalButton: {
+    backgroundColor: "#48A6A7",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    width: "100%",
+    borderRadius: 8,
+  },
+  modalButtonText: {
+    fontFamily: "Inter_500Medium",
+    fontSize: 16,
+    color: "#FFFFFF",
   },
 });
--- app/screens/streak/streak-list.tsx
+++ app/screens/streak/streak-list.tsx
@@ -1,9 +1,42 @@
+import { useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
 import { CalendarList } from "react-native-calendars";
+
 const StreakList = () => {
+  const router = useRouter();
   const [currentDate, setCurrentDate] = useState("");
   const [monthsSinceRegistered, setMonthsSinceRegistered] = useState(0);
+
+  const markedDates = [
+    { date: "2025-03-01", img: undefined },
+    { date: "2025-03-03", img: require("../../../assets/images/Pull day.png") },
+    { date: "2025-03-05", img: require("../../../assets/images/push-day.jpg") },
+    { date: "2025-03-07", img: undefined },
+    { date: "2025-03-10", img: require("../../../assets/images/guy1.png") },
+    { date: "2025-03-12", img: undefined },
+    { date: "2025-03-14", img: undefined },
+    { date: "2025-03-19", img: undefined },
+    {
+      date: "2025-03-21",
+      img: require("../../../assets/images/core-focus.jpg"),
+    },
+    { date: "2025-03-24", img: undefined },
+    {
+      date: "2025-03-26",
+      img: require("../../../assets/images/stretch-recovery.webp"),
+    },
+    { date: "2025-03-28", img: undefined },
+    {
+      date: "2025-03-31",
+      img: require("../../../assets/images/hiit-session.webp"),
+    },
+    { date: "2025-04-01", img: require("../../../assets/images/leg-day.jpg") },
+    { date: "2025-04-02", img: undefined },
+    { date: "2025-04-04", img: require("../../../assets/images/legday.png") },
+    { date: "2025-04-07", img: undefined },
+    { date: "2025-03-09", img: undefined },
+  ];
 
   useEffect(() => {
     let current = new Date();
@@ -24,38 +57,60 @@ const StreakList = () => {
         current={currentDate}
         pastScrollRange={monthsSinceRegistered}
         futureScrollRange={0}
-        markingType="custom"
-        markedDates={{
-          [currentDate]: {
-            marked: true,
-            dotColor: "#dc713d",
-            // dotColor: 'white',
-            // selected: true,
-            // selectedColor: "#dc713d",
-            customStyles: {
-              text: {
-                color: "#dc713d",
-              },
-            },
-          },
-          "2025-03-01": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-03": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-05": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-07": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-10": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-12": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-14": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-19": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-21": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-24": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-26": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-28": { selected: true, selectedColor: "#dc713d" },
-          "2025-03-31": { selected: true, selectedColor: "#dc713d" },
-          "2025-04-01": { selected: true, selectedColor: "#dc713d" },
-          "2025-04-02": { selected: true, selectedColor: "#dc713d" },
-          "2025-04-04": { selected: true, selectedColor: "#dc713d" },
-          "2025-04-07": { selected: true, selectedColor: "#dc713d" },
-          "2025-04-09": { selected: true, selectedColor: "#dc713d" },
+        dayComponent={({ date }) => {
+          const dateString = date?.dateString;
+          const matchedDate = markedDates.find(
+            (mark) => mark.date === (dateString ?? "")
+          );
+          return (
+            <View style={styles.dayContainer}>
+              {matchedDate ? (
+                <TouchableOpacity
+                  onPress={() =>
+                    router.push({
+                      pathname: "/screens/home/view-post",
+                      params: {
+                        id: data[1].id,
+                        name: data[1].name,
+                        fullName: data[1].fullName,
+                        email: data[1].email,
+                        postTitle: data[1].postTitle,
+                        description: data[1].description,
+                        time: data[1].time,
+                        volume: data[1].volume,
+                        likes: data[1].likes,
+                        comments: data[1].comments,
+                        date: data[1].date,
+                        profilePicture: data[1].profilePicture,
+                        postedPicture: data[1].postedPicture,
+                        sets: data[1].sets,
+                        records: data[1].records,
+                        isLiked: "false",
+                      },
+                    })
+                  }
+                >
+                  {matchedDate.img ? (
+                    <View style={styles.streakImageContainer}>
+                      <Text style={styles.streakImageText}>{date?.day}</Text>
+                      <Image
+                        source={matchedDate.img}
+                        style={styles.streakIcon}
+                      />
+                    </View>
+                  ) : (
+                    <View style={styles.streakCircle}>
+                      <Text style={styles.dayTextStreak}>{date?.day}</Text>
+                    </View>
+                  )}
+                </TouchableOpacity>
+              ) : dateString === currentDate ? (
+                <Text style={styles.currentDayText}>{date?.day}</Text>
+              ) : (
+                <Text style={styles.dayText}>{date?.day}</Text>
+              )}
+            </View>
+          );
         }}
       />
     </View>
@@ -73,4 +128,113 @@ const styles = StyleSheet.create({
   calendarContainer: {
     backgroundColor: "#F2F2F2",
   },
+  dayContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 40,
+  },
+  dayText: {
+    fontSize: 18,
+  },
+  currentDayText: {
+    color: "#48A6A7",
+    fontSize: 18,
+  },
+  currentDayDot: {
+    height: 30,
+    width: 30,
+    borderRadius: 18,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dayTextStreak: {
+    color: "#FFFFFF",
+    fontSize: 18,
+  },
+  streakCircle: {
+    height: 50,
+    width: 50,
+    borderRadius: 15,
+    backgroundColor: "#DC723D",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  streakImageContainer: {
+    position: "relative",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  streakIcon: {
+    width: 50,
+    height: 50,
+    borderRadius: 15,
+  },
+  streakImageText: {
+    position: "absolute",
+    color: "#FFFFFF",
+    fontSize: 18,
+    zIndex: 1,
+    opacity: 0.8,
+  },
 });
+
+type PostItem = {
+  id: string;
+  name: string;
+  fullName: string;
+  email: string;
+  active: string;
+  postTitle: string;
+  description: string;
+  profilePicture: any;
+  time: string;
+  volume: string;
+  postedPicture: any;
+  likes: string;
+  comments: string;
+  date: string;
+  sets: string;
+  records: string;
+};
+
+const data: PostItem[] = [
+  {
+    id: "1",
+    name: "mima79",
+    fullName: "John Smith Doe",
+    email: "mima@gmail.com",
+    active: "2 hours ago",
+    postTitle: "Leg Day!",
+    description: "No skip leg day",
+    profilePicture: require("../../../assets/images/guy1.png"),
+    time: "42 min",
+    volume: "3,780 kg",
+    postedPicture: require("../../../assets/images/legday.png"),
+    likes: "20 Likes",
+    comments: "0 comments",
+    date: "Tuesday, April 1, 2025 - 9:55am",
+    sets: "2",
+    records: "1",
+  },
+  {
+    id: "2",
+    name: "luffy",
+    fullName: "Monkey D. Luffy",
+    email: "luffykaizoku@gmail.com",
+    active: "5 hours ago",
+    postTitle: "Push day!",
+    description: "No pain no gain",
+    profilePicture: require("../../../assets/images/Pull day.png"),
+    time: "30 min",
+    volume: "4,780 kg",
+    postedPicture: require("../../../assets/images/legday.png"),
+    likes: "25 Likes",
+    comments: "4 comments",
+    date: "Wednesday, April 2, 2025 - 11:55am",
+    sets: "4",
+    records: "2",
+  },
+];
