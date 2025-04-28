75421e67880402120de4ba43600ebb389758b0cf
dhony-mendoza <dhony.mendoza@jg-corporation.com>
2025-04-28 11:04:56 +0800

    perf: added validation for email

--- app/screens/profile/settings/edit-profile.tsx
+++ app/screens/profile/settings/edit-profile.tsx
@@ -1,4 +1,10 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import ContainerSettings from "./container";
 import Input from "@/components/input-text";
@@ -7,16 +13,25 @@ import { router } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 
 import * as ImagePicker from "expo-image-picker";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { auth, db } from "@/utils/firebase-config";
+import { updateEmail, updateProfile } from "firebase/auth";
+import { setUserFromFirebase } from "@/redux/auth-slice";
+import { useAppDispatch } from "@/hooks/use-app-dispatch";
 
 const EditProfile = () => {
+  const dispatch = useAppDispatch();
   const user = useAppSelector((state) => state.auth.user);
+
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [exerciseImage, setExerciseImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
   const [errorMessage, setErrorMessage] = useState("");
 
   const [formData, setFormData] = useState({
-    username: user?.user_name || "",
+    username: user?.username || "",
     email: user?.email || "",
     password: "",
     confirm_password: "",
@@ -44,6 +59,54 @@ const EditProfile = () => {
     if (!result.canceled) {
       const { uri } = result.assets[0];
       setExerciseImage(uri);
+    }
+  };
+
+  const handleUpdateProfile = async () => {
+    setLoading(true);
+
+    const getUser = auth.currentUser;
+
+    if (!getUser?.uid) {
+      console.log("No authenticated user");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const userDoc = await getDoc(doc(db, "users", getUser.uid));
+
+      const { password, ...data } = formData;
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+
+        console.log(userData);
+
+        // update from firestore
+        await updateDoc(doc(db, "users", getUser.uid), data);
+
+        // update from authentication
+        await updateEmail(getUser, data.email);
+
+        console.log("Profile updated successfully.");
+      } else {
+        console.log("No such user document.");
+      }
+
+      // update from redux
+      dispatch(
+        setUserFromFirebase({
+          ...user,
+          uid: getUser.uid,
+          email: data.email,
+          username: data.username,
+        })
+      );
+    } catch (error) {
+      console.log("Error updating profile:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -149,17 +212,21 @@ const EditProfile = () => {
           marginTop: 20,
           width: "100%",
         }}
-        onPress={() => {}}
+        onPress={handleUpdateProfile}
       >
-        <Text
-          style={{
-            color: "#FFFFFF",
-            fontFamily: "Inter_600SemiBold",
-            fontSize: 16,
-          }}
-        >
-          Update
-        </Text>
+        {loading ? (
+          <ActivityIndicator size="small" color="#fff" />
+        ) : (
+          <Text
+            style={{
+              color: "#FFFFFF",
+              fontFamily: "Inter_600SemiBold",
+              fontSize: 16,
+            }}
+          >
+            Update
+          </Text>
+        )}
       </TouchableOpacity>
     </ContainerSettings>
   );
