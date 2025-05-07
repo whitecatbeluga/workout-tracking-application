ca80fbcdd523c24ed48a80d8563b0f2c6984840f
dhony-mendoza <dhony.mendoza@jg-corporation.com>
2025-05-07 13:37:02 +0800

    removed seeders button

--- app/screens/workout/index.tsx
+++ app/screens/workout/index.tsx
@@ -279,9 +279,9 @@ const WorkoutPage = () => {
                 <WorkoutHeader />
               </View>
 
-              <TouchableOpacity onPress={seedFirestore}>
+              {/* <TouchableOpacity onPress={seedFirestore}>
                 <Text>Seeders</Text>
-              </TouchableOpacity>
+              </TouchableOpacity> */}
 
               <View style={styles.routine}>
                 <Text style={styles.routineTxt}>Routines</Text>
